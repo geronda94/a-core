9a11796f9308f63977530231f05285b04d16cad1
geronda94 <iiiliev.igor@gmail.com>
2026-01-18 22:22:53 +0200

    123

--- identity.py
+++ identity.py
@@ -1,3 +1,4 @@
+# ~/a-core/identity.py
 import os
 import json
 import uuid
@@ -6,35 +7,26 @@ from datetime import datetime
 CONFIG_PATH = "config.json"
 
 def get_config():
-    """Загружает или создает расширенный конфиг устройства."""
     if os.path.exists(CONFIG_PATH) and os.path.getsize(CONFIG_PATH) > 0:
         try:
             with open(CONFIG_PATH, 'r') as f:
                 return json.load(f)
-        except json.JSONDecodeError:
+        except:
             pass
 
-    # Если файла нет или он битый — первичная инициализация
-    new_config = {
-        "device_id": f"phone_{uuid.uuid4().hex[:6]}",
+    # Первичная настройка
+    config = {
+        "device_id": f"id_{uuid.uuid4().hex[:6]}",
         "first_init": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "model": "Unknown",
         "resolution": "Unknown"
     }
-    
     with open(CONFIG_PATH, 'w') as f:
-        json.dump(new_config, f, indent=4)
-    return new_config
+        json.dump(config, f, indent=4)
+    return config
 
 def update_config(key, value):
-    """Метод для дополнения конфига (например, когда узнали разрешение)."""
     config = get_config()
     config[key] = value
     with open(CONFIG_PATH, 'w') as f:
         json.dump(config, f, indent=4)
-        
-        
-        
-if __name__ == "__main__":
-    cfg = get_config()
-    print("Current Config:", cfg)
